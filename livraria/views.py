54dd633cd79653dc0b740372ad2d6dc785811397
RafaAlbano <rafaelaalbano70@gmail.com>
2023-07-07 10:38:46 -0300

    commit

--- livraria/views.py
+++ livraria/views.py
@@ -6,9 +6,7 @@ from rest_framework.viewsets import ModelViewSet
 from livraria.models import Autor, Categoria, Editora, Livro
 from livraria.serializers import AutorSerializer, CategoriaSerializer, EditoraSerializer, LivroSerializer, LivroDetailSerializer
 
-class AutorViewSet(ModelViewSet):
-    queryset = Autor.objects.all()
-    serializer_class = AutorSerializer
+
 
 class CategoriaViewSet(ModelViewSet):
     queryset = Categoria.objects.all()
---
+++ livraria/views/__init__.py
@@ -0,0 +1,1 @@
+from livraria.models import Autor
