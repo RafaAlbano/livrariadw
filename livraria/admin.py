97341047efe956be2c0581e975d5209519ba7ea4
RafaAlbano <rafaelaalbano70@gmail.com>
2023-09-04 16:06:21 -0300

    passo 24

--- livraria/admin.py
+++ livraria/admin.py
@@ -1,11 +1,13 @@
 
 from django.contrib import admin
 
-from .models import Autor, Categoria, Editora, Livro
+from .models import Autor, Categoria, Editora, Livro, Compra
 
 admin.site.register(Autor)
 admin.site.register(Categoria)
 admin.site.register(Editora)
 admin.site.register(Livro)
+admin.site.register(Compra)
+
 
 
